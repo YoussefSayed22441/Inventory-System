3fdb1bee34d3fd79c020c64734a0ca4f0a0e7a88
Ezzatkarem <ztkarmabwmtr@gmail.com>
2026-05-03 13:11:28 +0300

    Add Infrastructure layer with InventoryDbContext, migrations, and composite key (ProductSuplier) configuration

---
+++ server/Inventory-System/Inventory-System.Infrastructure/Data/InventoryDbContext.cs
@@ -0,0 +1,34 @@
+﻿
+using Inventory_System.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Inventory_System.Infrastructure.Data
+{
+    public class InventoryDbContext :DbContext
+    {
+        public InventoryDbContext (DbContextOptions<InventoryDbContext> options)     : base(options)
+        {
+
+        }
+
+        // DbSets
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Notification>  Notifications { get; set; }
+        public DbSet<Supplier> Suppliers  { get; set; }
+        public DbSet<ProductSupplier> ProductSuppliers  { get; set; }
+        public DbSet<StockHistory> StockHistories { get; set; }
+
+
+        // Configure the model and apply configurations
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<ProductSupplier>()
+                .HasKey(ps => new { ps.ProductId, ps.SupplierId });
+        }
+
+    }
+
+}
