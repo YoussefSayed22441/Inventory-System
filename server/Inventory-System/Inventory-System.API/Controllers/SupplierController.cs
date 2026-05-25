8731238042f9e32456bacf3b98fc297cb4f5c506
khaled <khaledmaher923@gmail.com>
2026-05-25 21:07:29 +0300

    Supplier Infrastucture

---
+++ server/Inventory-System/Inventory-System.API/Controllers/SupplierController.cs
@@ -0,0 +1,22 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Inventory_System.API.Controllers
+{
+    [Route("api/[controller]")]
+    public class SupplierController : BaseApiController
+    {
+        private readonly IMediator _mediator;
+        public SupplierController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var response = await _mediator.Send(new GetAllSuppliersQuery());
+            return NewResult(response);
+        }
+}
