741ab2e1eae8e4432a0e4237e3bdfe80951ac9c9
khaled <khaledmaher923@gmail.com>
2026-07-13 19:48:15 +0300

    Updated

--- client/IMS-Front/src/services/axiosInstance.js
+++ client/IMS-Front/src/services/axiosInstance.js
@@ -1,12 +1,11 @@
 import axios from 'axios';
 
-const BASE_URL ='http://localhost:5280/api';
+const BASE_URL ='https://localhost:7125/api';
 
 /* ── Shared instance ─────────────────────────────────────────────────────── */
 const api = axios.create({
   baseURL: BASE_URL,
   headers: { 'Content-Type': 'application/json' },
-  withCredentials: true,
 });
 
 /* ── Request: inject Bearer token ────────────────────────────────────────── */
